8930eec48f9de0cfb8d4b58f7f2a341780aa22dd
Palmoliv <dimon_kg@mail.ru>
2016-12-19 00:22:49 +0600

    added code for run button

--- MO/SecantForm.cs
+++ MO/SecantForm.cs
@@ -20,7 +20,72 @@ namespace MethodsOfOpt
 
         private void runButton_Click(object sender, EventArgs e)
         {
-               
+            if (leftTextBox.Text == "" || rightTextBox.Text == "" || tolerTextBox.Text == "" || maxIterTextBox.Text == "")
+            {
+                MessageBox.Show("Не все данные введены!");
+            }
+            else
+            {
+                try
+                {
+                    resultTextBox.Text = "";
+                    functionResultTextBox.Text = "";
+                    toler2TextBox.Text = "";
+                    iterationsTextBox.Text = "";
+                    progressBar1.Value = 0;
+                    elapsedTimeTextBox.Text = "";
+                    label13.Text = "";
+                    decimal left = Decimal.Parse(leftTextBox.Text);
+                    decimal right = Decimal.Parse(rightTextBox.Text);
+                    decimal toler = Decimal.Parse(tolerTextBox.Text, NumberStyles.AllowExponent | NumberStyles.AllowDecimalPoint);
+                    int iter = Int32.Parse(maxIterTextBox.Text);
+                    int time = Int32.Parse(timeTextBox.Text);
+
+                    int elTime;
+                    decimal result;
+                    decimal y;
+                    decimal toler2;
+
+                    progressBar1.Visible = true;
+                    progressBar1.Maximum = iter + 1;
+                    SecantMethod.iterations = 0;
+                    SecantMethod.function = functionTextBox.Text;
+                    result = SecantMethod.SecantCount(left, right, toler, progressBar1, iter, time);
+                    y = Math.Round(SecantMethod.funcResult, 28);
+                    toler2 = (SecantMethod.endRight - SecantMethod.endLeft);
+                    iter = SecantMethod.iterations;
+                    elTime = SecantMethod.elapsedTime;
+                    resultTextBox.Text = result.ToString();
+                    functionResultTextBox.Text = y.ToString();
+                    toler2TextBox.Text = toler2.ToString("0e0");
+                    iterationsTextBox.Text = iter.ToString();
+                    elapsedTimeTextBox.Text = elTime.ToString();
+                    if (toler2 > toler)
+                    {
+                        label13.Text = "Результат с заданной точностью не достигнут!";
+                    }
+                    if (iter == 0)
+                    {
+                        label13.Text = "Ошибка";
+                        resultTextBox.Text = "";
+                        functionResultTextBox.Text = "";
+                        toler2TextBox.Text = "";
+                        iterationsTextBox.Text = "";
+                        progressBar1.Value = 0;
+                        elapsedTimeTextBox.Text = "";
+                    }
+                }
+                catch (ParserException ex)
+                {
+                    SecantMethod.tm.Stop();
+                    MessageBox.Show("Неверно введена функция!");
+                    progressBar1.Visible = false;
+                }
+                catch (FormatException ex)
+                {
+                    MessageBox.Show("Неверно введены начальные значения!");
+                }
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
