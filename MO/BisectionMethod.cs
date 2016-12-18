f1679e064bd7e58e9fc23e283601f3d3f102b5d0
Tilek <cirius-sam@list.ru>
2016-12-18 22:45:09 +0600

    bisection create

---
+++ MO/BisectionMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MethodsOfOpt
+{
+    static class BisectionMethod
+    {
+        static public int iterations;
+        static public string function;
+        static public decimal funcResult;
+        static public decimal endLeft;
+        static public decimal endRight;
+        static public int timerValue;
+        static public int maxTime;
+        static public int elapsedTime;
+        static public System.Timers.Timer tm;
+        
+
+    }
+}
