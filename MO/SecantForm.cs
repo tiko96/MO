33fb0e549b04d15a1a008caca2223da5c720f25d
Palmoliv <dimon_kg@mail.ru>
2016-12-19 00:20:40 +0600

    added interface for secant method

---
+++ MO/SecantForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+using info.lundin.math;
+using System.Globalization;
+
+namespace MethodsOfOpt
+{
+    public partial class SecantForm : Form
+    {
+
+
+        public SecantForm()
+        {
+            InitializeComponent();
+
+
+        }
+
+
+
+        private void runButton_Click(object sender, EventArgs e)
+        {
+               
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void leftTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void rightTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void maxTolTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void tolerTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           
+        }
+    }
+}
